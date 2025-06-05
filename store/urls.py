2b272a153b20f77e006dedabd58e63f7b3275c4a
vladkish <vladkish@users.noreply.github.com>
2025-06-05 15:00:56 +0300

    Displaying images via urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from products.views import index
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
