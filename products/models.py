fc12b270330c2e3eae9036311d9caea231991121
vladkish <vladkish@users.noreply.github.com>
2025-06-05 14:55:07 +0300

    Create admin and objects

--- products/models.py
+++ products/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return f"{self.title}"
+    
 
 # Class Product
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.title} | {self.price}"
     
 # Create class for photo.
 class ProductImage(models.Model):
